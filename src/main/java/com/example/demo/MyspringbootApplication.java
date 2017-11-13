f15c40d4669de4a91a6f121d6eda8e045521cf44
hczoop <274331927@qq.com>
2017-11-13 20:26:03 +0800

    去掉无用注释

--- src/main/java/com/example/demo/MyspringbootApplication.java
+++ src/main/java/com/example/demo/MyspringbootApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MyspringbootApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MyspringbootApplication.class, args);
 	}
